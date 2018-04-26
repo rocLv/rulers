f460f638a7fd8ac39fb61df30b48cfe19f7978ea
rocLv <wangqsh999@gmail.com>
2018-04-26 08:54:16 +0800

    initial Rulers Application class

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,5 +1,12 @@
 require "rulers/version"
 
 module Rulers
-  # Your code goes here...
+  class Application
+    def call(env)
+      [200,
+       {'Content-Type' => 'text/html'},
+       ["Hello, world!"]
+      ]
+    end
+  end
 end
