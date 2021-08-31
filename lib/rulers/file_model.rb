625a1f2b0fb847ba9f5dfda8b6f08fba60f0355e
rocLv <wangqsh999@gmail.com>
2021-08-31 21:43:07 +0800

    FileModel.create

--- lib/rulers/file_model.rb
+++ lib/rulers/file_model.rb
@@ -22,6 +22,32 @@ module Rulers
         @hash[name.to_s] = value
       end
 
+      def self.create(attrs)
+        hash = {}
+        hash["submitter"] = attrs["submitter"] || ""
+        hash["quote"] = attrs["quote"] || ""
+        hash["attribution"] = attrs["attribution"] || ""
+
+        files = Dir["db/quotes/*.json"]
+        names = files.map { |f| f.split("/")[-1] }
+        highest = names.map { |b| b.to_i }.max
+
+        id = highest + 1
+        File.open("db/quotes/#{id}.json", "w") do |f|
+          f.write <<TEMPLATE
+{
+      "submitter": "#{hash["submitter"]}",
+      "quote": "#{hash["quote"]}",
+      "attribution": "#{hash["attribution"]}"
+}
+TEMPLATE
+
+       end
+
+       FileModel.new "db/quotes/#{id}.json"
+
+      end
+
       def self.find(id)
         begin
           FileModel.new("db/quotes/#{id}.json")
